414c9c18aadca92024c0cd025b8636ca67fff081
IlyaM <ilya.mostolygin@gmail.com>
2021-12-10 14:22:27 +0300

    Homework level 3 lesson 5

---
+++ src/main/java/ru/geekbrains/hw/level3/lesson5/App.java
@@ -0,0 +1,44 @@
+package ru.geekbrains.hw.level3.lesson5;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.Semaphore;
+
+public class App {
+    public static final int CARS_COUNT = 4;
+    public static void main(String[] args) {
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
+
+        Semaphore smp = new Semaphore(CARS_COUNT / 2);
+
+        Race race = new Race(new Road(60), new Tunnel(smp), new Road(40));
+        Car[] cars = new Car[CARS_COUNT];
+
+        CyclicBarrier cb = new CyclicBarrier(CARS_COUNT + 1);
+        final CountDownLatch cdl = new CountDownLatch(CARS_COUNT);
+
+        for (int i = 0; i < cars.length; i++) {
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 10), cb, cdl);
+        }
+        for (Car car : cars) {
+            new Thread(car).start();
+        }
+
+        try {
+            cb.await();
+        } catch (InterruptedException | BrokenBarrierException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+
+        try {
+            cdl.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+    }
+}
